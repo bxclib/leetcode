5971473667cc4497e2bc0490aa9e9ded22151b22
bxclib <1652256031@qq.com>
2017-02-21 09:35:03 +0800

    Create Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        l=[]
+        l.append([])
+        result=[]
+        while l!=[]:
+            a=l.pop()
+            if len(a)==3:
+                result.append(a)
+            else:
+                for i in nums:
+                    if i not in a:
+                        a2=a[::]
+                        a2.append(i)
+                        l.append(a2)
+        return result
